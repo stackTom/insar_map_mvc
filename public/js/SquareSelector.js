f235b3173ac7b97437569bcf8d1e60f122eee5ac
stackTom <rtiky23@gmail.com>
2017-01-11 20:12:37 -0500

    Revert "Merge pull request #40 from stackTom/master"
    This reverts commit 359d462a9d75b99816f032e41052920debf214b2, reversing
    changes made to 19520dedcb76c086567d1789628e005b503cfa1f.

--- public/js/SquareSelector.js
+++ public/js/SquareSelector.js
@@ -100,7 +100,7 @@ function SquareSelector(map) {
 
         // If bbox exists. use this value as the argument for `queryRenderedFeatures`
         if (bbox) {
-            that.recolorMap();
+            that.recolorDataset();
         }
     };
 
@@ -124,28 +124,10 @@ function SquareSelector(map) {
         return pointIsInPolygon;
     };
 
-    this.recolorMap = function() {
-        if (that.bbox == null) {
-            return;
-        }
-
-        // haven't selected min and max, so exit
-        if (that.minIndex == -1 || that.maxIndex == -1) {
-            return;
-        }
-
-        // haven't changed since last recoloring? well dont recolor (only if it's the same area of course)
-        if (that.lastbbox == that.bbox && that.lastMinIndex == that.minIndex && that.lastMaxIndex == that.maxIndex) {
-            return;
-        }
-
+    this.recolorDatasetWithBoundingBoxAndMultiplier = function(box, multiplier) {
         if (that.recoloringInProgress) {
             return;
         }
-
-        that.lastMinIndex = that.minIndex;
-        that.lastMaxIndex = that.maxIndex;
-        that.lastbbox = that.bbox;
 
         if (that.map.map.getSource("onTheFlyJSON")) {
             that.map.map.removeSource("onTheFlyJSON");
@@ -158,8 +140,15 @@ function SquareSelector(map) {
             pointLayers.push(that.map.layers_[i].id);
         }
 
-        var pixelBoundingBox = [that.map.map.project(that.bbox[0]), that.map.map.project(that.bbox[1])];
-        var features = that.map.map.queryRenderedFeatures(pixelBoundingBox, { layers: pointLayers });
+        var features = null;
+        if (box) {
+            var pixelBoundingBox = [that.map.map.project(box[0]), that.map.map.project(box[1])];
+            features = that.map.map.queryRenderedFeatures(pixelBoundingBox, { layers: pointLayers });
+        // no bounding bax
+        } else {
+            features = that.map.map.queryRenderedFeatures({ layers: pointLayers });
+        }
+
         if (features.length == 0) {
             return;
         }
@@ -181,9 +170,9 @@ function SquareSelector(map) {
 
         var featuresMap = [];
 
-        var query = currentArea.unavco_name + "/";
+        var query = currentArea.properties.unavco_name + "/";
 
-	// may be placebo effect, but seems to speed up query from db. also
+        // may be placebo effect, but seems to speed up query from db. also
         // sort by p in ascending order so we match displacements with the features
         features.sort(function(a, b) {
             return a.properties.p - b.properties.p;
@@ -240,7 +229,7 @@ function SquareSelector(map) {
                 }
             }
         });
-        //console.log(query);        
+        //console.log(query);
         that.recoloringInProgress = true;
 
         $.ajax({
@@ -272,7 +261,7 @@ function SquareSelector(map) {
 
                     // calculate and render a linear regression of those dates and displacements
                     var result = calcLinearRegression(sub_displacements, sub_decimal_dates);
-                    var slope = result["equation"][0];
+                    var slope = result["equation"][0] * multiplier; // useful to get other derivatives such as position instead of velocity
                     var y = result["equation"][1];
                     // console.log("before " + curFeature.properties.m)
                     // console.log("slope is " + slope);
@@ -290,8 +279,6 @@ function SquareSelector(map) {
                     "data": geoJSONData
                 });
 
-                var colorScale = new ColorScale();
-
                 that.map.map.addLayer({
                     "id": "onTheFlyJSON",
                     "type": "circle",
@@ -299,7 +286,7 @@ function SquareSelector(map) {
                     "paint": {
                         'circle-color': {
                             property: 'm',
-                            stops: colorScale.colorsToMapboxStops(-0.02, 0.02, colorScale.jet)
+                            stops: that.map.colorScale.getMapboxStops()
                         },
                         'circle-radius': {
                             // for an explanation of this array see here:
@@ -320,5 +307,27 @@ function SquareSelector(map) {
                 console.log("failed " + xhr.responseText);
             }
         });
-    }
+    };
+
+    this.recolorDataset = function() {
+        if (that.bbox == null) {
+            return;
+        }
+
+        // haven't selected min and max, so exit
+        if (that.minIndex == -1 || that.maxIndex == -1) {
+            return;
+        }
+
+        // haven't changed since last recoloring? well dont recolor (only if it's the same area of course)
+        if (that.lastbbox == that.bbox && that.lastMinIndex == that.minIndex && that.lastMaxIndex == that.maxIndex) {
+            return;
+        }
+
+        that.lastMinIndex = that.minIndex;
+        that.lastMaxIndex = that.maxIndex;
+        that.lastbbox = that.bbox;
+
+        that.recolorDatasetWithBoundingBoxAndMultiplier(that.bbox, 1);
+    };
 }
